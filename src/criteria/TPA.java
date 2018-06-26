241771939841208d832fa76b1d2ddead86f7ae8f
Hasan Sözer <hasan.sozer@ozyegin.edu.tr>
2018-06-26 08:46:02 +0300

    dependendency matrix -> poset matrix
    for constructing a command for TPA

--- src/criteria/TPA.java
+++ src/criteria/TPA.java
@@ -12,18 +12,64 @@ public class TPA {
 	public static final String SCRIPT_PATH = "C:\\Users\\hasans\\Documents\\GitHub\\JBita\\lib\\";
 	public static final String TPA_SCRIPT = "tpa.r";
 	
-	public static void main(String args[]) {
+	private static Path scriptPath = null;
+	private static String posetMatrix = null;
 	
-		Path scriptPath = FileSystems.getDefault().getPath(SCRIPT_PATH, TPA_SCRIPT);
+	public static void main(String args[]) {
 		
-		String command = "poset1 <- matrix(c(1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,1),byrow=TRUE,ncol=4)\n";
-		command += "print(brute.force.count.linear.extensions(poset1))\n";
-		command += "print(tpa.approximation(poset1,1,0.1))\n";
+		/*
+		1,0,1,0
+		0,1,0,1
+		0,0,1,0
+		0,0,0,1
+		 */
+		boolean [][] depmatrix = new boolean[4][4];
+		depmatrix[0][0] = true;
+		depmatrix[0][2] = true;
+		depmatrix[1][1] = true;
+		depmatrix[1][3] = true;
+		depmatrix[2][2] = true;
+		depmatrix[3][3] = true;		
 		
-		runScriptandRemove(createScript(scriptPath, command));
+		setup(depmatrix);
+		getBruteForceCount();
+		getApproximation(1.0, 0.1); 
 	}
 	
-	private static Path createScript(Path scriptPath, String command) {
+	public static void setup(boolean dependencies[][]) {
+		scriptPath = FileSystems.getDefault().getPath(SCRIPT_PATH, TPA_SCRIPT);
+		posetMatrix = getPosetMatrix(dependencies);
+	}
+	
+	public static void getBruteForceCount() {
+		String command = posetMatrix
+					+ "print(brute.force.count.linear.extensions(poset))\n";
+		runScriptandRemove(createScript(command));
+	}
+	
+	public static void getApproximation(double epsilon, double delta) {
+		String command = posetMatrix
+				+ "print(tpa.approximation(poset,"
+				+ epsilon + "," + delta + "))\n";
+		runScriptandRemove(createScript(command));
+	}
+	
+	private static String getPosetMatrix(boolean dependencies[][]) {
+		String result = "poset <- matrix(c(";
+		
+		for (int i = 0; i < dependencies.length; i++)
+			for (int j = 0; j < dependencies.length; j++)
+				result += dependencies[i][j] ? "1," : "0," ;
+		 
+		result = result.substring(0,result.length()-1)
+				+ "),byrow=TRUE,ncol=" 
+				+ dependencies[0].length 
+				+ ")\n";
+		
+		return result;
+	}
+	
+	private static Path createScript(String command) {
 		Path tmpScriptPath = FileSystems.getDefault().getPath(SCRIPT_PATH, "tmp.r");
 		try {
 			Files.copy(scriptPath, tmpScriptPath);
